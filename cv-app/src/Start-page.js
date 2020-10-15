9836ac7ad263a609ed5076857ebc6088a8803588
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2020-10-15 12:50:09 +0200

    test

--- cv-app/src/Start-page.js
+++ cv-app/src/Start-page.js
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "./App.css";
 import Header from "./logo.js";
-import Home from "./Home page/Home-page";
+import Home from "./Home page/Home-page.js";
 
-export default function App() {
+function App({ logos, hardware }) {
   return (
     <div className="App">
       <h1>Welcome to my site</h1>
@@ -13,6 +13,26 @@ export default function App() {
       <button>Career</button>
       <button>Education</button>
       <button>Hobby</button>
+      <ul>
+        {logos.map((logos) => (
+          <li>{logos}</li>
+        ))}
+      </ul>
+      <div>
+        {hardware.map((hardware) => (
+          <>
+            <li>{hardware.id}</li>
+            <h1>{hardware.brand}</h1>
+            <h2>{hardware.name}</h2>
+            <p>{hardware.storage}</p>
+            <p>{hardware.read}</p>
+            <p>{hardware.write}</p>
+            <p>{hardware.dimensions}</p>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
+
+export default App;
--- cv-app/src/index.js
+++ cv-app/src/index.js
@@ -5,24 +5,80 @@ import "./index.css";
 import App from "./Start-page";
 import Home from "./Home page/Home-page";
 
-function Hello({ quote, message, number }) {
-  return (
-    <div>
-      <h1>{quote}</h1>
-      <p>{message}</p>
-      <p>{number}</p>
-    </div>
-  );
-}
+const computerLogo = [
+  "Asus logo",
+  "Apple logo",
+  "HP logo",
+  "Razer logo",
+  "Acer logo",
+];
+
+const computerHardware = [
+  {
+    id: "1",
+    name: "hard drive",
+    brand: "samsung",
+    storage: "128GB",
+    read: "1 GBps",
+    write: "500 Mbps",
+    dimensions: "2,5",
+  },
+  {
+    id: "2",
+    name: "hard drive",
+    brand: "Western Digital",
+    storage: "500GB",
+    read: "2 GBps",
+    write: "1 GBps",
+    dimensions: "2,5",
+  },
+  {
+    id: "3",
+    name: "hard drive",
+    brand: "HP",
+    storage: "1TB",
+    read: "3 GBps",
+    write: "256 Mbps",
+    dimensions: "3,5",
+  },
+];
+
+// function summerSale() {
+//   return (
+//     <div>
+//       <h1>Summersale is out!</h1>
+//     </div>
+//   );
+// }
+
+// function winterTime() {
+//   return (
+//     <div>
+//       <h1>Gaming is on!</h1>
+//     </div>
+//   );
+// }
+
+// const list = [1, 2, 3, 4, 5, 6, 7];
+
+// function Hello({ quote, message, number }) {
+//   return (
+//     <div>
+//       <h1>{quote}</h1>
+//       <p>{message}</p>
+//       <p>{number}</p>
+//     </div>
+//   );
+// }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <Hello
+    <App logos={computerLogo} hardware={computerHardware} />
+    {/* <Hello
       quote="Welcome to my site"
       message="This is dynamic string that will change"
       number={104854798}
-    />
+    /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
