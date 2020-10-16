f9f83bcee1edc09a557fea79b248060664d6b02a
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2020-10-17 00:43:04 +0200

    fagskolen i vestfold logo fix

--- cv-app/src/Education-card-component.js
+++ cv-app/src/Education-card-component.js
@@ -5,7 +5,7 @@ function EducationCardComponent({ EduData }) {
   const { name, duration, info, imgsrc, degree } = EduData;
   return (
     <div className="cardComponentWrapper">
-      <img src={imgsrc} alt="logo" />
+      <img className="logoSchool" src={imgsrc} alt="logo" />
       <h1>{name}</h1>
       <h2>{duration}</h2>
       <h3>{degree}</h3>
