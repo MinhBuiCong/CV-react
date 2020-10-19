123a6626db9ccf7eb67079f0e2ff5b6e984a6086
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2020-10-19 14:18:27 +0200

    Update Hobby-text-data.js
    adding text for hobbies

--- cv-app/src/Data/Hobby-text-data.js
+++ cv-app/src/Data/Hobby-text-data.js
@@ -1,15 +1,18 @@
 const HobbyTextData = [
   {
     id: 0,
-    info: "venner",
+    info:
+      "I enjoy travelling too new places and explore. I can't seem to sit still and just be at home. I like to do something all the time.",
   },
   {
     id: 1,
-    info: "buldring",
+    info:
+      "Bouldering is a big passion of mine. I have been doing it for almost two years now. The thing with bouldering is that you have to climb to the top of your route, but as harder the route gets, you need to learn how to position your body. Strength plays a big part of this sport too",
   },
   {
     id: 2,
-    info: "trening",
+    info:
+      "I do a lot of training beside bouldering. I mainly do calisthenics training. Calisthenics is a form of exercise that consist of movement that exercise large muscle group. Here we rely on the person's own bodyweight. My main goal is to learn the front lever.",
   },
 ];
 
